2d35ed1484c6219f49ddc84fc676be023b8e1bb1
jhlee <wisdom_13_@naver.com>
2023-02-28 16:32:14 +0900

    Create react project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { atom, selector, useRecoilValue } from 'recoil';
+import axios from 'axios';
+
+const todoIdState = atom({
+  key: 'todoIdState',
+  default: 1
+})
+
+const todoItemQuery = selector({
+  key: 'todoItemQuery',
+  get: async ({ get }) => {
+    const id = get(todoIdState);
+
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/todos/${id}`);
+    return response.data;
+  }
+})
 
 function App() {
+
+  const data = useRecoilValue(todoItemQuery);
+  console.log(data)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.title}
+      {data.userId}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import { RecoilRoot } from 'recoil';
+import Spinner from './Component/Spinner';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    {/* <Suspense fallback={<Spinner />}> */}
+    {/* <Suspense> */}
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+    {/* </Suspense> */}
+  </React.StrictMode >
 );
 
 // If you want to start measuring performance in your app, pass a function
