425bb8aee44302b23c91be3ff80db38eef4b1a4d
jhlee <wisdom_13_@naver.com>
2023-03-06 19:56:49 +0900

    TodoListl에 atom 연결하기

--- src/features/TodoList/index.tsx
+++ src/features/TodoList/index.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import styled from '@emotion/styled/macro';
-import { Todo } from './atom';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+
+import { selectedTodoState, Todo } from './atom';
+import { todoStatisicsModalOpenState } from '../TodoStatisticsModal/atom';
 
 const TodoItem = styled.li<{ done?: boolean; selected?: boolean }>`
   max-width: 100px;
@@ -44,17 +47,32 @@ interface Props {
 
 const TodoList: React.FC<Props> = ({ items }) => {
 
+  const selectedTodo = useRecoilValue(selectedTodoState);
+
+  const setSelectedTodo = useSetRecoilState(selectedTodoState);
+  const setTodoStatisticsModalOpen = useSetRecoilState(todoStatisicsModalOpenState);
+
+  const handleClick = (e: React.SyntheticEvent<HTMLLIElement>, todo: Todo) => {
+    e.stopPropagation();
+    setSelectedTodo(selectedTodo?.id === todo.id && selectedTodo.date === todo.date ? null : todo);
+  }
+
+  const handleTodoStatisticsModalOpen = (e: React.SyntheticEvent<HTMLLIElement>) => {
+    e.stopPropagation();
+    setTodoStatisticsModalOpen(true);
+  }
+
 
   return (
     <Base>
       {
         items.slice(0, 3).map((item, idx) => (
-          <TodoItem key={idx} done={item.done}>{item.content}</TodoItem>
+          <TodoItem key={idx} done={item.done} onClick={(e) => handleClick(e, item)}>{item.content}</TodoItem>
         ))
       }
       {
         items.length > 3 && (
-          <EtcItem>{`그 외 ${items.length - 3}`}</EtcItem>
+          <EtcItem onClick={(handleTodoStatisticsModalOpen)}>{`그 외 ${items.length - 3}`}</EtcItem>
         )
       }
       <EtcItem />
